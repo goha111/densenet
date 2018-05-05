168e0d5ad90fa4b912c03f41bce4a2ba7ea78edd
Hang Gong <gohakido@gmail.com>
2018-05-05 01:51:14 -0400

    change initializer

--- densenet.py
+++ densenet.py
@@ -96,7 +96,15 @@ class DenseNet(nn.Module):
         self.bn1 = nn.BatchNorm2d(nChannels)
         self.fc = nn.Linear(nChannels, nClasses)
 
-        self.apply(initializer)
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                m.bias.data.zero_()
 
     def forward(self, x):
         out = self.conv1(x)
